d5a79cbe2af7be4db5b422af417ff8aa8ef2dff7
kotoko <kotoko@users.noreply.github.com>
2022-01-08 21:52:21 +0100

    Small improvement

--- gen.sh
+++ gen.sh
@@ -8,6 +8,11 @@ PKGS='bzip2 cryptsetup curl ddrescue gnupg2 git gptfdisk gzip efibootmgr lvm2 ma
 # Source: https://github.com/void-linux/void-mklive/blob/master/build-x86-images.sh.in
 BASE_PKGS='dialog cryptsetup lvm2 mdadm void-docs-browse grub-i386-efi grub-x86_64-efi'
 
+# List of mirrors: https://docs.voidlinux.org/xbps/repositories/mirrors/
+REPO='https://mirrors.dotsrc.org/voidlinux/current'
+
+mkdir -p /etc/xbps.d
+echo "repository=${REPO}" >> /etc/xbps.d/repo.conf
 xbps-install --yes -Su xbps
 xbps-install --yes -Su
 xbps-install --yes -S git make
@@ -16,7 +21,4 @@ cd '/root'
 cd 'void-mklive/'
 make clean
 make
-
-# List of repository mirrors:
-#   https://docs.voidlinux.org/xbps/repositories/mirrors/
-./mklive.sh -a 'x86_64' -r 'https://mirrors.dotsrc.org/voidlinux/current' -p "${BASE_PKGS} ${PKGS}"
+./mklive.sh -a 'x86_64' -r "${REPO}" -p "${BASE_PKGS} ${PKGS}"
